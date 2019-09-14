d509d7c3919c70f7cdc1cb5e1fc6a375016e279e
iwannatto <iwannatto@gmail.com>
2019-09-14 23:29:09 +0900

    Add VGA, UART and tests for these

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,23 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use ros::println;
+use ros::{println, serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+    ros::test_panic_handler(info);
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    ros::test_panic_handler(info)
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
